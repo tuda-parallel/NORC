d3d2841661af00da1efa82e597d0ab136c5fec38
Ahmad Tarraf <ahmad.tarraf@tu-darmstadt.de>
2025-08-09 13:31:53 +0200

    Renamed tool to NORC

---
+++ analysis/norc/__init__.py
@@ -0,0 +1,21 @@
+# This file is part of the FTIO software
+#
+# Copyright (c) 2023-2024, Technical University of Darmstadt, Germany
+#
+# This software may be modified and distributed under the terms of a BSD-style license.
+# See the LICENSE file in the base directory for details.
+
+__title__ = "NORC"
+__version__ = "0.0.1"
+__license__ = "BSD 3"
+__description__ = "NORC: Noise Resilient Hardware Counters in HPC"
+__copyright__ = (
+    "Copyright (c) 2025-2026 Technical University of Darmstadt, Darmstadt, Germany"
+)
+__documentation_link__ = f"https://github.com/tuda-parallel/NORC/tree/v{__version__}/docs"
+__current_version_api__ = "https://pypi.org/pypi/NORC"
+__developed_by_html__ = (
+    'NORC is developed by <a href="https://www.parallel.informatik.tu-darmstadt.de/">'
+)
+__support_email__ = "ahmad.tarraf@tu-darmstadt.de"
+__repo__ = "https://github.com/tuda-parallel/NORC"
